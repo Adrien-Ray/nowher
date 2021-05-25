0b2cfb4b6b50a464255e823c6a379d292be790b4
Adrien-Ray <forpub@accesdenied.net>
2021-05-25 16:33:58 +0200

    js

--- scripts/dyslexia.js
+++ scripts/dyslexia.js
@@ -8,6 +8,10 @@ document.write("Chaîne d'origine : " + chaine + "<br>");
 document.write("Chaîne traitée : " + chaine.replace(reg,"<span style='background-color:yellow'>$1</span>") + "<br>"); */
 
 //recup en string les contenus visés
+let testContent = "azertyuiopmlkjhgfdsqwxcvbn";
 //événement
 //si événement function
       //regex sur contenu visé
+      let ifdyslex = new RegExp("(b)",'<span class="red">b</span>');
+      let skiAuraSurDocument = testContent.replace(ifdyslex,testContent);
+console.log('skiAuraSurDocument');
