d966d1f7de3628a1f8beaa4c61c4a4f0e949a149
Nicolasmaes <n.maes@codeur.online>
2021-05-25 20:56:08 +0200

    continuité du menu hamburger

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,10 @@ const menu = document.getElementById('classic_menu');
 let cross1 = document.getElementById('barre1');
 let cross2 = document.getElementById('barre2');
 let cross3 = document.getElementById('barre3');
+const btn2 = document.getElementById('rubrique1');
+const btn3 = document.getElementById('rubrique2');
+const sousMenu1 = document.getElementById('sous-menu1');
+const sousMenu2 = document.getElementById('sous-menu2');
 let counter = 'closed';
 
 
@@ -13,6 +17,8 @@ btn.addEventListener('click',unroll);
 function unroll(){
     if (counter == 'closed') {
         menu.style.display = 'block';
+        sousMenu1.style.display = 'block';
+        sousMenu2.style.display = 'block';
         counter = 'open';
         console.log(counter);
         cross1.classList.toggle("cross1transform");
@@ -21,6 +27,8 @@ function unroll(){
         }
         else{
         menu.style.display = 'none';
+        sousMenu1.style.display = 'none';
+        sousMenu2.style.display = 'none';
         counter = 'closed';
         console.log(counter);
         cross1.style.width = '60px';
@@ -48,3 +56,51 @@ function unroll(){
     menu.style.display = 'block'; 
 } */
 
+btn2.addEventListener('click',unroll2);
+
+
+function unroll2(){
+    if (counter == 'closed') {
+        sousMenu1.style.display = 'block';
+        counter = 'open';
+        console.log(counter);
+        }
+        else{
+        sousMenu1.style.display = 'none';
+        counter = 'closed';
+        console.log(counter);
+
+/*         cross1.style.left = '0';
+        cross1.style.top = '0';
+        cross1.classList.toggle("nico");
+        cross1.style.transform = 'rotate(0deg)'; */
+
+/*         cross3.style.transform = 'rotate(0deg)';
+        cross3.style.width = '60px';
+        cross3.style.bottom = '0'; */
+    }
+}
+
+btn3.addEventListener('click',unroll3);
+
+function unroll3(){
+    if (counter == 'closed') {
+        sousMenu2.style.display = 'block';
+        counter = 'open';
+        console.log(counter);
+        }
+        else{
+        sousMenu2.style.display = 'none';
+        counter = 'closed';
+        console.log(counter);
+
+/*         cross1.style.left = '0';
+        cross1.style.top = '0';
+        cross1.classList.toggle("nico");
+        cross1.style.transform = 'rotate(0deg)'; */
+
+/*         cross3.style.transform = 'rotate(0deg)';
+        cross3.style.width = '60px';
+        cross3.style.bottom = '0'; */
+    }
+}
