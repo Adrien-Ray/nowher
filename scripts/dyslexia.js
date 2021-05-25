5f58420a24a2508e392a3a04407150e48e3b1586
Adrien-Ray <forpub@accesdenied.net>
2021-05-25 16:01:50 +0200

    js dislex

---
+++ scripts/dyslexia.js
@@ -0,0 +1,8 @@
+// b et p surligner différentes color
+
+// ex
+
+/* var chaine="Les chiens et les chiennes, les chats et les oiseaux";
+var reg=new RegExp("(chien)", "g");
+document.write("Chaîne d'origine : " + chaine + "<br>");
+document.write("Chaîne traitée : " + chaine.replace(reg,"<span style='background-color:yellow'>$1</span>") + "<br>"); */
