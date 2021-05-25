ed417ad4be0784586e5f5ac489b1e4e13e9dd6a9
acs <a.bederiat@codeur.online>
2021-05-25 15:50:41 +0200

    branchadam

---
+++ pages/articledemence.php
@@ -0,0 +1,13 @@
+<?php include "../includes/header.php"; ?>
+
+
+
+    <div class = "article__">
+        <h5>La demence qu'est ce que c'est ?</h5>
+        <img src="../assets/images/icons/imgarticle.svg" alt="" class="article__img">
+    </div>
+    
+
+
+<?php include "../includes/footer.php"; ?>
+
