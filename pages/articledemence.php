4612acb5f0becfdf3ce2ead4d3df994f38693ec5
acs <a.bederiat@codeur.online>
2021-05-26 13:46:52 +0200

    article

--- pages/articledemence.php
+++ pages/articledemence.php
@@ -2,12 +2,63 @@
 
 
 
-    <div class = "article__">
-        <h5>La demence qu'est ce que c'est ?</h5>
-        <img src="../assets/images/icons/imgarticle.svg" alt="" class="article__img">
+    <div class = "articledemence">
+        <div>
+            <h4 class = "articledemence__sous_titre">La demence qu'est ce que c'est ?</h4>
+            <h4>Définition générale</h4>
+        </div>
+            <img src="../assets/images/icons/imgarticle.svg" alt="" class="article__img">
     </div>
+    <div class ="articledemence__texte">
+    <p>
+    La démence est un trouble neurocognitif qui se caractérise par des déficits cognitifs divers
+    variant en fonction du type de démence (définis par leur cause). La maladie d’Alzheimer est le 
+    type de démence le plus fréquent.</p> 
+    <p> Il arrive à chacun d’entre nous de commettre des erreurs ou des étourderies. Il peut arriver que 
+    l’on oublie ou l’on a bien pu déposer un objet ou un autre ou que nous avons déjà raconter la 
+    même histoire à un de nos proches. Ces comportements peuvent généralement être expliqués 
+    par une saturation d’informations due à nos modes de vie actifs et stressants et ne sont bien 
+    souvent pas des signes de démence. Plus les gens vieillissent, plus la mémoire aura tendance à 
+    fonctionner de manière différente. Il se peut, par exemple, que le cerveau ne traite plus aussi 
+    rapidement les informations qui lui parviennent. Ce type de modifications de fonctionnement du 
+    cerveau est normal et n’interfère que peu avec la vie quotidienne. A l’inverse, la démence est 
+    invalidante et n’est pas attribuée au processus normal de vieillissement.</p>
+     <p> Bien que la démence ait tendance à se manifester différemment chez chaque individu, nous 
+    pouvons recenser un certain nombre de symptômes fréquents commun aux personnes 
+    atteintes : </p>
     
-
+    <ul>
+        <li> Une perte progressive de la mémoire des événements survenus récemment et l’incapacité d’assimiler de nouvelles informations </li>
+        <li> Une propension à se répéter, à perdre des objets, à s’égarer dans des endroits familiers ...</li>
+        <li> Une difficulté à manifester un jugement et une désintégration des capacités de raisonnement ...</li>
+        <li> Une tendance accrue à l’irritabilité, à l’anxiété, à la dépression, à la confusion et à l’agitation ...</li>
+        <li> Des difficultés à communiquer et à manier la langue </li>
+        <li> Des changements du comportement, des sautes d’humeurs </li>
+        <li> Une diminution de la capacité à se concentrer ou à rester concentrer </li>
+        <li> Une incapacité ou des difficultés à planifier et effectuer des tâches en plusieurs étapes (comme compléter des documents administratifs). </li>
+    </ul>
+    <p> Par ailleurs, certains types de démences peuvent toucher des personnes plus jeunes que d’autres </p>
+    <h4> Principaux types de démence</h4>
+    <ul>
+        <li> La maladie d’Alzheimer </li>
+        <li> La démence à corps de Lewy </li>
+        <li> La démence vasculaire </li>
+        <li> La démence fronto-temporale </li>
+        <li> La démence mixte </li>
+    </ul>
+    <h4> Caractéristiques & troubles associés </h4>
+    <p> Les personnes atteintes de démences sont susceptibles de ne pas avoir conscience de leur perte de mémoire. Elles sont aussi très sensibles au stress physique et psychologique, qui peut 
+    accroître leurs déficits et les symptômes qui y sont associés.</p>
+    <p> Le départ d’une démence varie selon la cause qui en est à l’origine mais il est généralement très 
+    tardif. Elle est plus fréquente après 85 ans, et rarement présente chez les enfants ou les 
+    adolescents. On estime que la démence de type Alzheimer touche environ 2 à 4% de la 
+    population de plus de 65 ans. Les démences d’autres types sont beaucoup moins fréquents. Ces 
+    dernières peuvent être stables, progressives ou en rémission.</p>
+    </div>
+    <div class = "articledemence__source"> Sources
+    <a href="https://ressourcessante.salutbonjour.ca/condition/getcondition/demence">Ressources Santé</a>
+    <a href="http://www.psychomedia.qc.ca/diagnostics/qu-est-ce-que-la-demence">Psychomédia</a>
+    </div>
 
 <?php include "../includes/footer.php"; ?>
 
---
+++ pages/donnees-chiffrees.php
@@ -0,0 +1,15 @@
+<?php include "../includes/header.php"; ?>
+
+<div class = "donnees-chiffrees">
+        <div>
+            <h4 class = "donnees-chiffrees__titre">La demence qu'est ce que c'est ?</h4>
+        </div>
+            <img src="../assets/images/icons/image-graphique.svg" alt="" class="article__img">
+    </div>
+
+
+
+
+
+
+<?php include "../includes/footer.php"; ?>
