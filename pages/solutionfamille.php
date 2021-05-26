b5f8e50a045c2cfc508cfcd42630c881e28b0127
acs <a.bederiat@codeur.online>
2021-05-26 16:52:12 +0200

    articles

---
+++ pages/solutionfamille.php
@@ -0,0 +1,21 @@
+<?php include "../includes/header.php"; ?>
+
+
+<div class ="solution">
+    <h4>Solution famille : </h4>
+        <ul>
+            <li><a href="https://www.francealzheimer.org/">france alzheimer</a></li>
+            <li><a href="https://www.pour-les-personnes-agees.gouv.fr/">Pour les personnes agées</a></li>
+            <li><a href="http://www.lamaisondesaidants.com/site2018/wp-content/uploads/2014/09/Guide-de-laidant-2017-Alzheimer.pdf">La maison des aidants</a></li>
+            <li><a href="https://www.capretraite.fr/aide-a-domicile/maintien-a-domicile/">Cap retraite</a></li>
+        </ul>
+</div>
+
+
+
+
+
+
+
+
+<?php include "../includes/footer.php"; ?>
