63f9facfcdadf2a0a63f088df35d5982805634c1
Nicolasmaes <n.maes@codeur.online>
2021-05-27 15:53:52 +0200

    footer terminé

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,10 @@
 </div>
+<footer>
+<a>Copyright@2021 - NOWHER</a>
+<a href="../pages/mentionslégales.php">Mentions légales</a>
+<a href="../pages/contact.php">Contact</a>
+<a href="#websitelogo">Haut de page</a>
+</footer>
 <?php
 if ($_SESSION['js1'] === '1') {
 ?>
--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,3 @@
 <?php require '../includes/header.php';?>
-<div id="aa">azazaz breg bdfwgfg dzreswtr <a href="">huiui boph ohd iubl</a><b>b bipuboh^d sq d</b>
-<span>coucou<b>lkjlkj</b>jhkjh</span>
-</div>
+
 <?php require '../includes/footer.php';?>
