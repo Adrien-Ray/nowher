91abc7e3d26232bfd222806d06e27a539cb384fe
Adrien-Ray <forpub@accesdenied.net>
2021-05-30 10:42:29 +0200

    netoyage fontsize.js

--- scripts/fontsize.js
+++ scripts/fontsize.js
@@ -19,6 +19,4 @@ document.addEventListener('DOMContentLoaded',fontSize());
 function fontSize(){
     document.body.style.fontSize="2rem";
     document.body.style.backgroundColor="green";
-    document.getElementsByClassName('sidebar').style.fontSize="0.5rem";
-    document.getElementsByClassName('sidebar').style.backgroundColor="red";
     } 
