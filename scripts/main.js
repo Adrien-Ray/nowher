9d3374e3d2b8ad1cedd9415627f1558e9a0648a8
Hervé <h.richard.codeur.onlinem>
2021-05-31 14:06:00 +0200

    push

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 7b7f64bb8990d900881ab55951da5490e4c9fd7e
 // MENU HAMBURGER
 const btn = document.getElementById('hamb_menu_btn');
 const dropbtn1 = document.getElementById('menumobile');
@@ -34,7 +30,3 @@ function unroll(){
  
     }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 7b7f64bb8990d900881ab55951da5490e4c9fd7e
