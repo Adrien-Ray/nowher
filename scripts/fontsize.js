5b39796ab81e72e8db5b76220a2cfb4520d03d11
Adrien-Ray <forpub@accesdenied.net>
2021-05-31 16:18:27 +0200

    zoom finit

--- scripts/fontsize.js
+++ scripts/fontsize.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded',fontSize());
+/* document.addEventListener('DOMContentLoaded',fontSize());
 function fontSize(){
     document.body.className += "jsFontSize";
     document.body.style.zoom=1.4;this.blur();
-} 
+}  */
 /****************************************************************
  ***************  NE FONCTIONNE PAS SOUS FIREFOX ****************
  **** https://developer.mozilla.org/en-US/docs/Web/CSS/zoom *****
