17b7a1e70c1c5ee5fb0d30dcb472bb3abdb73dd8
Nicolasmaes <n.maes@codeur.online>
2021-05-31 15:36:11 +0200

    ajout des target blank

--- pages/solutionfamille.php
+++ pages/solutionfamille.php
@@ -6,9 +6,9 @@
     <h4>Solution famille : </h4>
         <ul>
             <li><a href="https://www.francealzheimer.org/">france alzheimer</a></li>
-            <li><a href="https://www.pour-les-personnes-agees.gouv.fr/">Pour les personnes agées</a></li>
-            <li><a href="http://www.lamaisondesaidants.com/site2018/wp-content/uploads/2014/09/Guide-de-laidant-2017-Alzheimer.pdf">La maison des aidants</a></li>
-            <li><a href="https://www.capretraite.fr/aide-a-domicile/maintien-a-domicile/">Cap retraite</a></li>
+            <li><a href="https://www.pour-les-personnes-agees.gouv.fr/" target="_blank">Pour les personnes agées</a></li>
+            <li><a href="http://www.lamaisondesaidants.com/site2018/wp-content/uploads/2014/09/Guide-de-laidant-2017-Alzheimer.pdf" target="_blank">La maison des aidants</a></li>
+            <li><a href="https://www.capretraite.fr/aide-a-domicile/maintien-a-domicile/" target="_blank">Cap retraite</a></li>
         </ul>
 </div>
 <?php require '../includes/footer.php';?>
