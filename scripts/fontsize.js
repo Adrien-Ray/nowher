a7452b085556a36ab17b16f498ffbae7d0400417
Adrien-Ray <forpub@accesdenied.net>
2021-05-31 16:13:56 +0200

    test

--- scripts/fontsize.js
+++ scripts/fontsize.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded',fontSize());
 function fontSize(){
     document.body.className += "jsFontSize";
     document.body.style.zoom=1.4;this.blur();
-    document.body.style.fontSize="150%";
 } 
 /****************************************************************
  ***************  NE FONCTIONNE PAS SOUS FIREFOX ****************
