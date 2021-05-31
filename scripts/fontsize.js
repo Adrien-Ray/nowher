8770ebff33258ce4ca31e2d70b8de8a96ea30ffe
Adrien-Ray <forpub@accesdenied.net>
2021-05-31 15:34:30 +0200

    fontsize

--- scripts/fontsize.js
+++ scripts/fontsize.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-/* fonction btn size ! */
-/* let text = document.getElementsByClassName('articledemence__texte');
-let fontSizeBtn = document.getElementById('fontSizeBtn');
-let fontSizeBtnValue = 1;
-fontSizeBtn.addEventListener('click' ,function fontChangeSize()  {
-if (fontSizeBtnValue == 0) {
-    text.style.fontSize= '30px' ;
-    fontSizeBtnValue = 1;
-    console.log(fontSizeBtnValue);
-} else if (fontSizeBtnValue == 1){
-    fontSizeBtnValue = 0;
-    text.style.fontSize= '100px' ;
-    console.log(fontSizeBtnValue);
-} else {
-    console.log(fontSizeBtnValue);
-}
-}); */
-
-    document.body.style.fontSize="2rem";/* 
-    document.body.getElementsByClassName('sidebar').style.fontSize="0.5rem";
-    document.body.getElementsByClassName('sidebar').style.backgroundColor="red"; */
-=======
 document.addEventListener('DOMContentLoaded',fontSize());
 function fontSize(){
     document.body.className += "jsFontSize";
@@ -30,4 +7,3 @@ function fontSize(){
  ***************  NE FONCTIONNE PAS SOUS FIREFOX ****************
  **** https://developer.mozilla.org/en-US/docs/Web/CSS/zoom *****
  ****************************************************************/
->>>>>>> c7153cf39782b5c60c51e4ae4cf61f326886f2de
