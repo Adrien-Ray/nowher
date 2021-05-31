ff2ddafb8809a0ca2bb03dc516503cce70cea8d8
Hervé <h.richard.codeur.onlinem>
2021-05-31 13:55:12 +0200

    push

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,7 +3,7 @@
 <div class="sidebar">
 
 <a class="problemea" id="fontSizeBtn" href="../scripts/session.php?js1=0&url=<?php echo "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];?>">A-/A+</a>
-<a href="../scripts/session.php?js2=0&url=<?php echo "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];?>"  class="problemeb"><img src="../assets/images/pictures/lecture.png" alt="blabla"></a>
-<a class="problemea" href="../scripts/session.php?js3=0&url=<?php echo "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];?>"><img src="../assets/images/pictures/P.png" alt="blabla"></a>
+<a href="../scripts/session.php?js2=0&url=<?php echo "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];?>"  class="problemeb"><img src="../assets/images/pictures/lecture.png" class="imgSizeFix" alt="blabla"></a>
+<a class="problemea" href="../scripts/session.php?js3=0&url=<?php echo "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];?>"><img src="../assets/images/pictures/P.png" class="imgSizeFix" alt="blabla"></a>
 
 </div>
--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,4 @@
 <?php require '../includes/header.php';?>
-<?php include "../includes/sidebar.php"; ?>
+
 
 <?php require '../includes/footer.php';?>
