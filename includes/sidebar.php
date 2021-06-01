861d9c74a242df904e67d4bae738a9209ea35bec
Hervé <h.richard.codeur.onlinem>
2021-06-01 14:07:26 +0200

    mention legales update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,11 +1,5 @@
 <div class="sidebar">
-<<<<<<< HEAD
-<a title="fonction mal-voyant<?php if (strpos($_SERVER['HTTP_USER_AGENT'], "Firefox")) {echo " : désolé, cette fonction n'est pas possible sous firefox";} ?>" class="problemea" id="fontSizeBtn" href="../scripts/session.php?js1=0&url=<?php echo "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];?>"> <img src="../assets/images/pictures/zoom.png" alt="zoom"></a>
-<a title="site lisible par synthétiseur vocal" href="#"  class="problemeb"><img src="../assets/images/pictures/lecture.png" alt="lecture vocal"></a>
-<a title="fonction dyslexie" class="problemea" href="../scripts/session.php?js3=0&url=<?php echo "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];?>"><img src="../assets/images/pictures/pnew.png" alt="dyxlexie"></a>
-=======
 <a title="fonction mal-voyant" class="problemea" id="fontSizeBtn" href="../scripts/session.php?js1=0&url=<?php echo "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];?>"> <img src="../assets/images/pictures/zoom.png" alt="icone zoom"></a>
 <a title="site lisible par synthétiseur vocal" href="#"  class="problemeb"><img src="../assets/images/pictures/lecture.png" alt="blabla"></a>
 <a title="fonction dyslexie" class="problemea" href="../scripts/session.php?js3=0&url=<?php echo "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];?>"><img src="../assets/images/pictures/pnew.png" alt="blabla"></a>
->>>>>>> d685802c19535ae30870622fde8a8fc88c9c7430
 </div>
