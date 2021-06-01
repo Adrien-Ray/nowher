e2305692ca1362dfbbe7a677f025af90bce47616
Nicolasmaes <n.maes@codeur.online>
2021-06-01 15:23:38 +0200

    travail du footer qui disparait et changement du breakpoint du sidebar à 768px

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,10 @@
 </div>
-<?php 
-include "../includes/sidebar.php"; 
- ?>
-<footer class="footer" <?php if($show==0){echo 'id="coucou"' ;}?> > 
+<?php include "../includes/sidebar.php"; ?>
+<footer class="footer" <?php if($show==0){echo 'id="coucou"' ;}?> >
 <p>Copyright@2021 - NOWHER</p>
-<a href="../pages/mentionslégales.php" aria-label="Accéder à la page mentions légales" role="link">Mentions légales</a>
+<a href="../pages/mentionslégales.php" aria-label="Accéder à la page mentions légales" role=link>Mentions légales</a>
 <a href="../pages/contact.php" aria-label="Accéder à la page contact" role=link>Contact</a>
-<a href="#websitelogo" aria-label="Revenir en haut de la page" role=link>Haut de page</a>
+<a href="#websitelogo" aria-label="Revenir en haut de page" role=link>Haut de page</a>
 </footer>
 <?php
 if (isset($_SESSION['js1']) AND $_SESSION['js1'] === '1') {
@@ -20,6 +18,6 @@ if (isset($_SESSION['js3']) AND $_SESSION['js3'] === '1') {
 <?php
 }
 ?>
-   <script src="../scripts/main.js"></script>
+<script src="../scripts/main.js"></script>
 </body>
 </html>
