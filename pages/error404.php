1bdfc795b9e8234993f436e8748a189f7facab7d
Adrien-Ray <forpub@accesdenied.net>
2021-06-02 15:16:50 +0200

    finition post-prod

--- pages/error404.php
+++ pages/error404.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/my_work/NOWHER/styles/404.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="/nowher/styles/404.css">
+    <title>error 404</title>
 </head>
 <body class="body_moon">
 
