33f8772aa4ab78f6cf6449083f25183c71e2b0cb
Nicolasmaes <n.maes@codeur.online>
2021-06-03 16:55:21 +0200

    version qui fonctionne

--- pages/error404.php
+++ pages/error404.php
@@ -5,11 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/nowher/styles/404.css">
-<<<<<<< HEAD
-    <title>Document</title>
-=======
     <title>error 404</title>
->>>>>>> 14a5c989ab1cf6b88e7568bc71ab4b1917044159
 </head>
 <body class="body_moon">
 
